9aa78eb48feb99cea7b8de8817e0d55d8c893d86
Vedanth Shenoy <vshenoy3@uncc.edu>
2025-02-18 13:50:53 -0500

    Made the good calc even better by allowing the application to continue if there is an input that is not valid

--- good_calc.py
+++ good_calc.py
@@ -14,17 +14,20 @@ def divide(a, b):
 
 def calculator():
     operations = {'+': add, '-': subtract, '*': multiply, '/': divide}
-    
-    try:
-        a = float(input("Enter first number: "))
-        op = input("Enter operation (+, -, *, /): ")
-        b = float(input("Enter second number: "))
+    while True:
+        try:
+            a = float(input("Enter first number: "))
+            op = input("Enter operation (+, -, *, /): ")
+            b = float(input("Enter second number: "))
         
-        if op in operations:
-            print("Result:", operations[op](a, b))
+            if op in operations:
+                print("Result:", operations[op](a, b))
+            else:
+                print("Error: Invalid operation")
+        except ValueError:
+            print("Error: Invalid input")
+            continue
         else:
-            print("Error: Invalid operation")
-    except ValueError:
-        print("Error: Invalid input")
+            break
 
 calculator()
